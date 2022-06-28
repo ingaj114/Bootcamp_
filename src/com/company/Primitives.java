eca30be99f005014b56ff21ec60e8b48530a2371
ingaj114 <ingaj114@gmail.com>
2022-06-28 14:07:33 +0300

    Initial commit

---
+++ src/com/company/Primitives.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Primitives {
+    public static void main(String[] args) {
+        byte myNumber = 8;
+        short myShortNum = 25;
+        int myInt = 512;
+        long myLongNum = 98654l;
+        float myFloatNum = 2.16f;
+        double myDoubleNum = 2.1512d;
+        char myChar = 'C';
+        boolean myBool = true;
+
+        System.out.println("Byte is " + myNumber);
+        System.out.println("Short is " + myShortNum);
+        System.out.println("Integer is " + myInt);
+        System.out.println("Long number is " + myLongNum);
+        System.out.println("Float number is " + myFloatNum);
+        System.out.println("Double is " + myDoubleNum);
+        System.out.println("Character is " + myChar);
+        System.out.println("Boolean is " + myBool);
+    }
+}
